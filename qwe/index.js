5b1d8a3b6376ef4ce1a40f265005cdd610ef851e
temaflexible <128032000+temaflexible@users.noreply.github.com>
2023-06-14 18:39:46 +1000

    Update index.js

--- qwe/index.js
+++ qwe/index.js
@@ -1,24 +1,40 @@
-const truck = document.getElementById("truck")
-const cactus = document.getElementById("cactus")
+const truck = document.getElementById("truck");
+const cactus = document.getElementById("cactus");
 
-document.addEventListener("keydown", function(event){
-    jump()
-})
+let score = 0
+const container = document.querySelector('h2')
 
-function jump() {
-    if(truck.classList != "jump"){
-        truck.classList.add("jump")
+document.addEventListener("keydown", function (event) {
+  document.addEventListener("keydown", function (event) {
+    jump();
+  });
+
+  function jump() {
+    if (truck.classList != "jump") {
+      truck.classList.add("jump");
     }
-    setTimeout (function() {
-        truck.classList.remove("jump")
-    }, 500)
-}
 
-let crushed = setInterval (function(){
-    let trucktop = parseInt(window.getComputedStyle(truck).getPropertyValue("top"))
-    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+    setTimeout(function () {
+      truck.classList.remove("jump");
+      container.innerText = "Очки: "  + score
+    }, 500);
+  }
 
-    if(cactusleft < 50 && cactusleft > 0 && trucktop>=140) {
-        alert("GAME OVER")
+  setTimeout(function(){
+    score++
+  }, 500)
+
+  let crushed = setInterval(function () {
+    let trucktop = parseInt(
+      window.getComputedStyle(truck).getPropertyValue("top")
+    );
+    let cactusleft = parseInt(
+      window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+
+    if (cactusleft < 50 && cactusleft > 0 && trucktop >= 140) {
+      alert("GAME OVER");
+      score = 0
     }
-}, 10)
+  }, 10);
+});
